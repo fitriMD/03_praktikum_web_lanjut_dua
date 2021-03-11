3c8be9ef460720cdffd92008ebd3296412d75ade
fitriMD <mutiara.devi027@gmail.com>
2021-03-11 17:25:33 +0700

    praktikum 2 js3

--- resources/views/respon.blade.php
+++ resources/views/respon.blade.php
@@ -3,11 +3,9 @@
 @section('content')
 
 <div class="row tm-welcome-content">
-          <h2 class="white-text tm-handwriting-font tm-welcome-header"><img src="img/header-line.png" alt="Line" class="tm-header-line">&nbsp;Welcome To&nbsp;&nbsp;<img src="img/header-line.png" alt="Line" class="tm-header-line"></h2>
-          <h2 class="gold-text tm-welcome-header-2">Fila's Cafe</h2>
-          <p class="gray-text tm-welcome-description" style="text-align:center">Halo Fila's Lovers, silahkan pesan menu favorit kalian. Nikmati hari - hari penuh canda tawa kalian
-                                                      dengan spesial menu dari Fila's Cafe. Jangan lewatkan juga promo - promo menarik dari kami. Pastikan kalian juga stay tune dengan
-                                                      informasi yang diberikan oleh Fila's Cafe melalui sosmed. Jika kalian butuh positif vibes yuk datang aja ke <span class="gold-text"> FILA'S CAFE.</span> Kami tunggu yaaa
+          <h2 class="white-text tm-handwriting-font tm-welcome-header"><img src="img/header-line.png" alt="Line" class="tm-header-line">&nbsp;Thank You To&nbsp;&nbsp;<img src="img/header-line.png" alt="Line" class="tm-header-line"></h2>
+          <h2 class="gold-text tm-welcome-header-2">Fila's Cafe Customers</h2>
+          <p class="gray-text tm-welcome-description" style="text-align:center">Hai Fila's Cafe Customers terimakasi banyak atas pesan yang kamu kirimkan, tunggu balasan dari kami ya.
                                                       <span class="gold-text"> HAVE A NICE DAY!</span>
  
 
@@ -72,7 +70,7 @@
         <section class="tm-section">
           <div class="row">
             <div class="col-lg-12 tm-section-header-container">
-              <h2 class="tm-section-header gold-text tm-handwriting-font"><img src="img/logo.png" alt="Logo" class="tm-site-logo"> Quotes</h2> 
+              <h2 class="tm-section-header gold-text tm-handwriting-font"><img src="img/logo.png" alt="Logo" class="tm-site-logo">Note</h2> 
               <div class="tm-hr-container"><hr class="tm-hr"></div> 
             </div>  
           </div>          
@@ -82,7 +80,7 @@
                 <img src="img/menu-board.png" alt="Menu board" class="tm-daily-menu-img">      
               </div>            
               <div class="col-lg-8 col-md-8"> 
-                <p> <span class="white-text"><b>Percayalah dari kegagalan kamu akan tau betapa pentingnya sebuah usaha dan doa.</b></span></p>
+                <p> <span class="white-text"><b>Kritikan dan saran membangun dari kalian adalah awal kesuksesan kami.</b></span></p>
               </div>
             </div>
           </div>          
